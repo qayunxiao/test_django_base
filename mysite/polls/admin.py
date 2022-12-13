f358250d79b323a3f90282bd1ec7b853223b1051
alvinsss <6449694@qq.com>
2022-12-13 11:01:35 +0800

    完善admin管理后台

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question,Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text','pub_date']
+    search_fields = ['question_text']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text','votes','question']
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice,ChoiceAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,7 +2,10 @@ from django.db import models
 #module 数据库ORM   py-->ORM ---> 数据库驱动-->sqlite3
 #ORM把写SQL语句屏蔽了 可操作类方法一样操作数据库
 #首先安装orm方式创建表
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,8 +13,16 @@ class Question(models.Model):
     question_desc = models.TextField(null=True,default='')
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
