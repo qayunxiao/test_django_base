e2c8dbf3a5a7af9d378e0e2d8cac7cf43352ccce
alvinsss <6449694@qq.com>
2022-12-13 11:17:06 +0800

    自定义模板路径

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"polls","html")],#自定义模板路径
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
